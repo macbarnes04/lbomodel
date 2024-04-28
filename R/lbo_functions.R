0d21b7465cc8ed12852ed8a9211a7c8477810c49
Mac Barnes <b.mac@wustl.edu>
2024-04-27 19:41:14 -0500

    test 2 of adding files to git

---
+++ R/lbo_functions.R
@@ -0,0 +1,14 @@
+#' A basic function
+#'
+#' This function allows you to add two number together
+#' @param x first number to add 
+#' @param y second number to add
+#' @keywords add, sum 
+#' @export
+#' @examples
+#' function1(1, 2)
+
+function1 <- function(x, y){
+  result = x + y
+  print(result)
+}
